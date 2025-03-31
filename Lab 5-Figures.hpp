fe6aef45d71fff9c2b771cf5e8afa80e9589ca5f
codeE99 <csherman3102@gmail.com>
2025-03-31 04:54:21 -0400

    Create Lab 5-Figures.hpp
    Our header file for lab 5-figures. We do something similar to lab 3, but spread over multiple files and with procedural functions. We define these functions in figures.cpp and implement them in figuresInput.cpp.

---
+++ Lab 5-Figures.hpp
@@ -0,0 +1,12 @@
+#ifndef FIGURES_HPP //This protects our header from multiple inclusions
+#define FIGURES_HPP
+
+//Our header file. Functions will be declared here
+
+void hollowSquare(int size, char symbol);
+void filledSquare(int size, char symbol);
+void slashForward(int size, char symbol);
+void slashBackward(int size, char symbol);
+void X(int size, char symbol);
+
+#endif
